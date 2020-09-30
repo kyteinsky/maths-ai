d27d6b9ce5f15c1efbb73e316910529661729d08
kyte <kyteinsky@gmail.com>
2020-10-01 00:31:15 +0530

    fixed heroku issues

--- sheets.py
+++ sheets.py
@@ -3,7 +3,7 @@ from oauth2client.service_account import ServiceAccountCredentials
 from pprint import pprint
 
 scope = ["https://spreadsheets.google.com/feeds",'https://www.googleapis.com/auth/spreadsheets',"https://www.googleapis.com/auth/drive.file","https://www.googleapis.com/auth/drive"]
-creds = ServiceAccountCredentials.from_json_keyfile_name("google-credentials.json", scope)
+creds = ServiceAccountCredentials.from_json_keyfile_name("app/google-credentials.json", scope)
 client = gspread.authorize(creds)
 sheet = client.open("logging").sheet1  # Open the spreadhseet
 
